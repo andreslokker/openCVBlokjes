648892a155c1919a3084791f90964b6eb8abb582
andre <andreslokker1@gmail.com>
2020-02-18 12:38:14 +0100

    line detection pictures

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,36 @@
 
 using namespace cv;
 
+int blockSize = 2;
+int apertureSize = 3;
+double k = 0.04;
+
 int main(int argc, char** argv) {
     namedWindow("normal picture");
     namedWindow("img detected");
+    namedWindow("multiply");
+    namedWindow("edged pictures");
     String imageName("pictures/picture.png");
     Mat img = imread(imageName);
     Mat imgHsv;
     Mat imgTreshold;
+    Mat imgMultiply;
+    Mat imgEdges;
+    
     cvtColor(img, imgHsv, COLOR_BGR2HSV);
     inRange(imgHsv, Scalar(0, 30, 0), Scalar(10, 255, 255), imgTreshold);
+    cvtColor(imgTreshold, imgTreshold, COLOR_GRAY2BGR);
+    cvtColor(imgTreshold, imgTreshold, COLOR_BGR2HSV);
+    multiply(imgTreshold, imgHsv, imgMultiply);
+    Canny(imgMultiply, imgEdges, 100, 200);
+    cvtColor(imgEdges, imgEdges, COLOR_GRAY2BGR);
+    cvtColor(imgEdges, imgEdges, COLOR_BGR2HSV);
 
     while(true) {
         imshow("normal picture", img);
         imshow("img detected", imgTreshold);
+        imshow("multiply", imgMultiply);
+        imshow("edged pictures", imgEdges);
 
         char key = (char) waitKey(30);
         if (key == 'q' || key == 27) {
