885e44460a2dd149cde8813e2a4ca550fb032ad0
andre <andreslokker1@gmail.com>
2020-02-18 13:35:32 +0100

    code

--- src/main.cpp
+++ src/main.cpp
@@ -17,14 +17,28 @@ int main(int argc, char** argv) {
     Mat img = imread(imageName);
     Mat imgHsv;
     Mat imgTreshold;
+    Mat imgTresholdHsv;
     Mat imgMultiply;
     Mat imgEdges;
     
+    blur(img, img, Size(5,5));
     cvtColor(img, imgHsv, COLOR_BGR2HSV);
     inRange(imgHsv, Scalar(0, 30, 0), Scalar(10, 255, 255), imgTreshold);
-    cvtColor(imgTreshold, imgTreshold, COLOR_GRAY2BGR);
-    cvtColor(imgTreshold, imgTreshold, COLOR_BGR2HSV);
-    multiply(imgTreshold, imgHsv, imgMultiply);
+    erode(imgTreshold, imgTreshold, getStructuringElement(MORPH_RECT, Size(9,9), Point(-1,-1)));
+    dilate(imgTreshold, imgTreshold, getStructuringElement(MORPH_RECT, Size(9,9), Point(-1,-1)));
+    imgTreshold.copyTo(imgTresholdHsv);
+    
+    std::vector< cv::Point2f > imgCorners;
+    goodFeaturesToTrack(imgTreshold, imgCorners, 4, 0.01, 10);
+
+    for( size_t i = 0; i < imgCorners.size(); i++ ) {
+        cv::circle(imgTreshold, imgCorners[i], 10, cv::Scalar( 140. ), -1 );
+    }
+
+    
+    cvtColor(imgTresholdHsv, imgTresholdHsv, COLOR_GRAY2BGR);
+    cvtColor(imgTresholdHsv, imgTresholdHsv, COLOR_BGR2HSV);
+    multiply(imgTresholdHsv, imgHsv, imgMultiply);
     Canny(imgMultiply, imgEdges, 100, 200);
     cvtColor(imgEdges, imgEdges, COLOR_GRAY2BGR);
     cvtColor(imgEdges, imgEdges, COLOR_BGR2HSV);
