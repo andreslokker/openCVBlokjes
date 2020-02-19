0c4d4795355ecee032ec8e2f180daad66e9bd081
Jochem Grootherder <j.grootherder@student.han.nl>
2020-02-19 11:59:46 +0100

    saturation sliders

--- src/main.cpp
+++ src/main.cpp
@@ -7,23 +7,25 @@ using namespace cv;
 
 int hue_max = 179;
 int hRangeMax = 20;
+int saturationMax = 255;
+int sRangeMax = 255;
 
 int hValue = 0;
 int hRange = 0;
 int sValue = 0;
+int sRange = 0;
 int vValue = 0;
 
 Mat img, imgHsv, imgTreshold;
 
 void change_hsv(int, void*)
-	{
-//		cvtColor(img, imgHsv, COLOR_BGR2HSV);
-		if(hValue - hRange <= 0) hValue = 0;
-		if(hValue + hRange >= 179) hValue = 179;
-	    inRange(imgHsv, Scalar(hValue - hRange, 30, 0), Scalar(hValue + hRange, 255, 255), imgTreshold);
-//		cvtColor(imgHsv, COLOR_)
-//	    inRange(imgHsv, Scalar(0, 30, 0), Scalar(10, 255, 255), imgTreshold);
-	}
+{
+	if(hValue - hRange <= 0) hValue = 0;
+	if(hValue + hRange >= 179) hValue = 179;
+	if(sValue - sRange <= 0) sValue = 0;
+	if(sValue + sRange >= 255) sValue = 255;
+    inRange(imgHsv, Scalar(hValue - hRange, sValue - sRange, 0), Scalar(hValue + hRange, sValue + sRange, 255), imgTreshold);
+}
 
 int main(int argc, char** argv) {
     namedWindow("normal picture");
@@ -33,11 +35,13 @@ int main(int argc, char** argv) {
     pyrDown(img, img, Size(img.cols/2, img.rows/2));
 
     cvtColor(img, imgHsv, COLOR_BGR2HSV);
-    inRange(imgHsv, Scalar(0, 30, 0), Scalar(10, 255, 255), imgTreshold);
+    inRange(imgHsv, Scalar(hValue - hRange, sValue - sRange, 0), Scalar(hValue + hRange, sValue + sRange, 255), imgTreshold);
 
     namedWindow("Trackbars", CV_WINDOW_AUTOSIZE);
     createTrackbar("Hue", "Trackbars", &hValue, hue_max, change_hsv);
     createTrackbar("Hue range", "Trackbars", &hRange, hRangeMax, change_hsv);
+    createTrackbar("Saturation", "Trackbars", &sValue, saturationMax, change_hsv);
+    createTrackbar("Saturation range", "Trackbars", &sRange, sRangeMax, change_hsv);
 
     while(true) {
         imshow("normal picture", img);
