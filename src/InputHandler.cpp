e85d82584ea518a4e964acf6c929f846656d459f
Jochem Grootherder <j.grootherder@student.han.nl>
2020-02-27 15:19:13 +0100

    inputHandler update voor batch

--- src/InputHandler.cpp
+++ src/InputHandler.cpp
@@ -2,65 +2,110 @@
 #include <fstream>
 #include <iostream>
 
-InputHandler::InputHandler(std::mutex* inputQueueMutex) : inputQueueMutex(inputQueueMutex), thread1(&InputHandler::run, this) {
+InputHandler::InputHandler(std::mutex* inputQueueMutex) :
+		inputQueueMutex(inputQueueMutex), thread1(&InputHandler::run, this)
+{
 
 }
 
-InputHandler::~InputHandler() {
+InputHandler::~InputHandler()
+{
 
 }
 
-void InputHandler::parseLine(const std::string& line) {
-    if(line.find("#") != std::string::npos) {
-        return;
-    }
-    std::string input = line.substr(0, line.length());
-    int count = 0;
-    for (int i = 0; i < input.length(); i++) {
-        if (isspace(input.at(i))) {
-            count++;
-        }
-    }
-    std::string shape;
-    std::string color;
-    if(count == 0) {
-        shape = input;
-    } else if (count == 1) {
-        shape = input.substr(0, input.find(" "));
-        color = input.substr(input.find(" ") + 1);
-    } else if (count == 2) {
-        shape = input.substr(0, input.find(" ") + input.find(" ") + 2);
-        color = input.substr(input.find(" ") + input.find(" ") + 3);
+void InputHandler::parseLine(const std::string& line)
+{
+	if(line == "exit")
+	{
+		std::exit(0);
+	}
+	std::string input = line.substr(0, line.find("#"));
+	int count = 0;
+	for (int i = 0; i < input.length(); i++)
+	{
+		if (isspace(input.at(i)))
+		{
+			count++;
+		}
+	}
+	std::string shape;
+	std::string color;
+	if (count == 1)
+	{
+		bool shapeFound = false;
+		for (unsigned short i = 0; i < input.length(); i++)
+		{
+			if (!shapeFound && !isspace(input.at(i)))
+			{
+				shape += input.at(i);
+			}
+			else if (shapeFound)
+			{
+				color += input.at(i);
+			}
+			else if (isspace(input.at(i)))
+			{
+				shapeFound = true;
+			}
+		}
+	}
+	else if (count == 2)
+	{
+		bool shapeFound = false;
+		bool firstWhitespaceFound = false;
+		for (unsigned short i = 0; i < input.length(); i++)
+		{
+			if (!shapeFound)
+			{
 
-    }
-    inputQueueMutex->lock();
-    inputQueue.push(std::make_pair(shape, color));
-    inputQueueMutex->unlock();
+				if (isspace(input.at(i)) && firstWhitespaceFound)
+				{
+					shapeFound = true;
+				}
+				if (isspace(input.at(i)))
+					firstWhitespaceFound = true;
+				shape += input.at(i);
+			}
+			else if (shapeFound)
+			{
+				color += input.at(i);
+			}
+		}
+	}
+	inputQueueMutex->lock();
+	inputQueue.push(std::make_pair(shape, color));
+	inputQueueMutex->unlock();
 }
 
-void InputHandler::readFile(const std::string& filename) {
-    std::ifstream myfile(filename);
+void InputHandler::readFile(const std::string& filename)
+{
+	std::ifstream myfile(filename);
 	std::string input;
-	if (myfile.is_open()) {
-		while (std::getline(myfile, input)) {
-            parseLine(input);
+	if (myfile.is_open())
+	{
+		while (std::getline(myfile, input))
+		{
+			parseLine(input);
 		}
 	}
 }
 
-void InputHandler::readCommandLine() {
-    std::string input;
-    std::getline(std::cin, input);
-    parseLine(input);
+void InputHandler::readCommandLine()
+{
+	std::string input;
+	std::getline(std::cin, input);
+	parseLine(input);
 }
 
-void InputHandler::run() {
-    while(true) {
-        readCommandLine();
-    }
+void InputHandler::run()
+{
+	while (true)
+	{
+		readCommandLine();
+	}
 }
 
-
- std::queue<std::pair<std::string, std::string> >& InputHandler::getInputQueue() {
-    return inputQueue;
- }
+std::queue<std::pair<std::string, std::string> >& InputHandler::getInputQueue()
+{
+	return inputQueue;
+}
