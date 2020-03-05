971796897238a08f3e5b1da84dd9c6e724c9dbb2
Jochem Grootherder <j.grootherder@student.han.nl>
2020-03-05 11:21:04 +0100

    Doxygen added

--- src/ArgumentParser.hpp
+++ src/ArgumentParser.hpp
@@ -2,7 +2,12 @@
 #define ARGUMENTPARSER_HPP
 
 #include <string>
-
+/**
+ * @brief Enum Mode
+ * Different types of modes the application can run in.
+ * Interactive: custom input while application is running
+ * Batch: input from batch file
+ */
 enum Mode {
     NONE,
     INTERACTIVE,
@@ -12,10 +17,38 @@ enum Mode {
 
 class ArgumentParser {
     public:
+        /**
+        * @brief Construct a new Argument Parser object
+        * 
+        */
         ArgumentParser();
+        
+        /**
+         * @brief Destroy the Argument Parser object
+         * 
+         */
         virtual ~ArgumentParser();
+        
+        /**
+         * @brief Parse arguments given when application is launched
+         * 
+         * @param argc 
+         * @param argv 
+         */
         void parseArguments(int argc, char** argv);
+       
+        /**
+         * @brief Get the Mode object
+         * 
+         * @return Mode 
+         */
         Mode getMode();
+        
+        /**
+         * @brief Get the Location object
+         * 
+         * @return const std::string& 
+         */
         const std::string& getLocation() const;
     private:
         std::string location;
--- src/ColorDetector.hpp
+++ src/ColorDetector.hpp
@@ -10,9 +10,26 @@
 
 class ColorDetector {
     public:
+        /**
+         * @brief Construct a new Color Detector object
+         * 
+         */
         ColorDetector();
+        
+        /**
+         * @brief Destroy the Color Detector object
+         * 
+         */
         virtual ~ColorDetector();
-
+        
+        /**
+         * @brief Filter out the color which is given from the colorconfiguration and present it in a new image
+         * 
+         * @param image input image
+         * @param colorConfiguration the configuration values from the color which is to be found 
+         * @param timer  a timer to count how long it takes to find the specific color
+         * @return cv::Mat the output image in which the searched color is represented in
+         */
         cv::Mat detectColor(cv::Mat& image,const ColorConfiguration& colorConfiguration, Timer& timer);
 };
 
