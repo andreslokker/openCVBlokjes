fa0e722fe1e22d30f82b956f3e722a8c4903b5af
Jochem Grootherder <j.grootherder@student.han.nl>
2020-03-06 12:35:59 +0100

    doxygen readded

--- src/ArgumentParser.hpp
+++ src/ArgumentParser.hpp
@@ -12,10 +12,38 @@ enum Mode {
 
 class ArgumentParser {
     public:
+        /**
+        * @brief Construct a new Argument Parser object
+        * 
+        */
         ArgumentParser();
+
+        /**
+         * @brief Destroy the Argument Parser object
+         * 
+         */
         virtual ~ArgumentParser();
+
+        /**
+         * @brief Parse arguments given when application is launched
+         * 
+         * @param argc 
+         * @param argv 
+         */
         void parseArguments(int argc, char** argv);
+
+         /**
+         * @brief Get the Mode object
+         * 
+         * @return Mode 
+         */
         const Mode& getMode() const;
+
+        /**
+         * @brief Get the Location object
+         * 
+         * @return const std::string& 
+         */
         const std::string& getLocation() const;
     private:
         std::string location;
--- src/Configure.hpp
+++ src/Configure.hpp
@@ -22,12 +22,53 @@ typedef struct {
 
 class Configure {
     public:
+         /**
+         * @brief Construct a new Configure object
+         * 
+         * @param objectDetector 
+         */
         explicit Configure(ObjectDetector* objectDetector);
+        
+        /**
+         * @brief Destroy the Configure object
+         * 
+         */
         virtual ~Configure();
+        
+        /**
+         * @brief startConfiguration, starts up the configuration to update the configuration file
+         * 
+         */
         void startConfiguration();
+
+         /**
+         * @brief onTrackbar Trackbar function, updates the values for the colorConfiguration
+         * 
+         * @param int
+         * @param void*
+         */
         static void onTrackbar(int, void*);
+
+        /**
+         * @brief Line parser, parses the line and puts them into the colorConfiguration
+         * Seperates each section of the input by using a " " as delimeter
+         * @param input Input line which is parsed
+         * @param lineCount The line number which is read, puts the input into this position of the colorConfiguration
+         */
         static void parseLine(std::string input, unsigned short lineCount);
+
+        /**
+         * @brief Read the configuration file line by line and calls parseLine function for each line
+         * 
+         */
         void readConfiguration();
+
+        /**
+         * @brief Get the Color Configuration object
+         * 
+         * @param color string which defines which color should be returned 
+         * @return ColorConfiguration 
+         */
 	static ColorConfiguration getColorConfiguration(std::string& color);
 
     private:
